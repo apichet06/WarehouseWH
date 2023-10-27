99048edf2da3e90de543f91c19825743ac35d8e9
apichet06 <apichet06@gmail.com>
2023-10-27 16:59:17 +0700

    7rd commint

--- src/components/productList/productlistForm.tsx
+++ src/components/productList/productlistForm.tsx
@@ -9,22 +9,33 @@ interface Props {
 }
 interface DataProduct {
     productID: string
-
+    pImages: string
+    productName: string
+    productDescription: string
+    productType: {
+        typeName: string
+    },
+    unitPrice: string
 }
-
 
 export default function ProductlistForm(props: Props) {
 
     const field = {
-        productID: ''
+        productID: '',
+        pImages: '',
+        productName: '',
+        productDescription: '',
+        productType: {
+            typeName: ''
+        },
+        unitPrice: ''
     }
 
-
+    const result: any = JSON.parse(localStorage.getItem("resulte") || "{}")
     const { api } = props
     const [data, setDate] = useState<DataProduct[]>([]);
     const [showData, setShowData] = useState<DataProduct>(field);
-
-
+    const [qty, setQty] = useState(1);
     // สร้างฟังก์ชันสำหรับการจัดการข้อความยาว
     function truncateText(text: string, maxLength: number) {
         if (text.length <= maxLength) {
@@ -54,20 +65,57 @@ export default function ProductlistForm(props: Props) {
 
 
 
+
+
+    const handleSave = async () => {
+        // ตรวจสอบว่า qty อยู่ในช่วงที่ระบบรับได้
+        if (qty >= 1 && qty <= 9999999) {
+            try {
+                const Formdata = new FormData();
+                Formdata.append("productID", showData.productID);
+                Formdata.append("QTYWithdrawn", qty.toString());
+                Formdata.append("withdrawnBy", result.userID.toString());
+                Formdata.append("unitPrice", showData.unitPrice);
+                const response = await axios.post(api + '/Picking_goodsDetailAPI', Formdata, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+
+                if (response.status === 200) {
+                    handleClose();
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            console.log("Qty is out of range.");
+        }
+    }
+
+
+
+
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
 
     const handleShow = async (productID: string) => {
         const pro = await data.find((item: any) => item.productID === productID)
+
         if (pro) {
             setShowData({
                 productID: pro.productID,
+                pImages: pro.pImages,
+                productName: pro.productName,
+                productDescription: pro.productDescription,
+                productType: { typeName: pro.productType.typeName },
+                unitPrice: pro.unitPrice
             })
         }
         setShow(true);
+        setQty(1)
     }
-
 
     return (
         <>
@@ -95,14 +143,14 @@ export default function ProductlistForm(props: Props) {
                                     <ListGroup.Item style={{ fontSize: '12px' }}> {truncateText(item.productDescription, 22)}</ListGroup.Item>
                                 </ListGroup>
                                 <Card.Body>
-                                    <Button size="sm" variant="primary" onClick={() => (handleShow(item.productID))}>เลือกเบิก</Button>
+                                    <Button size="sm" variant="warning" onClick={() => (handleShow(item.productID))}>Preview</Button>
                                 </Card.Body>
                             </Card>
                         </Col>
                     ))}
                 </Row>
             </Container>
-            <ProductlistMadals api={api} handleClose={handleClose} show={show} showData={showData} />
+            <ProductlistMadals api={api} handleClose={handleClose} show={show} showData={showData} qty={qty} setQty={setQty} handleSave={handleSave} />
 
         </>
     )
--- src/components/productList/productlistMadals.tsx
+++ src/components/productList/productlistMadals.tsx
@@ -1,36 +1,92 @@
-import { Button, Modal } from "react-bootstrap"
+import { Button, Col, Container, Form, Image, InputGroup, Modal, Row } from "react-bootstrap"
 
 interface Props {
     api: string
     handleClose: () => void
     show: boolean
     showData: {
-        productID: string;
+        productID: string
+        pImages: string
+        productName: string
+        productDescription: string
+        productType: {
+            typeName: string
+        }
+        unitPrice: string
     }
+    qty: number
+    setQty: (value: React.SetStateAction<number>) => void
+    handleSave: () => void
 }
 
 export default function ProductlistMadals(props: Props) {
-    const { api, handleClose, show, showData } = props
+    const { api, handleClose, show, showData, qty, setQty, handleSave } = props
+
+
+    const handlePlus = () => {
+        setQty(qty + 1); // เพิ่ม qty ขึ้น 1
+    }
+
+    const handleReduce = () => {
+        if (qty > 1) {
+            setQty(qty - 1); // ลด qty ลง 1 ถ้า qty มากกว่า 1
+        }
+    }
+
+
     return (
         <>
-            {api}
+
+
             <Modal
                 show={show}
                 onHide={handleClose}
+                size="lg"
                 backdrop="static"
                 keyboard={false}
             >
-                <Modal.Header closeButton>
+                <Modal.Header>
                     <Modal.Title>{showData.productID}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    I will not close if you click outside me. Don not even try to press
-                    escape key.
+                    <Container>
+                        <Row className="justify-content-center">
+                            <Col md={5}>
+                                <Image src={api + '/ImagesPathAPI/' + showData.pImages} width={"100%"} thumbnail className="shadow-sm" />
+                            </Col>
+                            <Col md={7}>
+                                <Row>
+                                    <Col md={12} className="my-2">
+                                        <strong ><u>{showData.productName}</u></strong>
+                                        <h6 className="m-3">ประเภท : {showData.productType.typeName}</h6>
+                                        <h6 className="m-3">รายละเอียด : {showData.productDescription}</h6>
+                                        <h6 className="m-3">คารา : {showData.unitPrice.toLocaleString()} บาท </h6>
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col md={2} xs={3} className="text-center my-3" >จำนวน</Col>
+                            <Col md={3} xs={5} className="text-center my-3" style={{ marginTop: "auto" }}>
+                                <InputGroup  >
+                                    <Button variant="secondary" id="button-addon1" onClick={handleReduce} size="sm">-</Button>
+                                    <Form.Control value={qty} className="text-center" size="sm" maxLength={3} onChange={(e) => {
+                                        let newQty = parseInt(e.target.value, 10);
+                                        if (isNaN(newQty) || newQty === 0) {
+                                            // ถ้าผู้ใช้กรอกเลข 0 หรือไม่ใช่ตัวเลขให้ตั้งเป็น 1 แทน
+                                            newQty = 1;
+                                        }
+                                        setQty(newQty);
+                                    }} />
+                                    <Button variant="secondary" id="button-addon1" onClick={handlePlus} size="sm">+</Button>
+                                </InputGroup>
+                            </Col>
+                            <Col md={2} xs={4} className="text-center my-3" style={{ marginTop: "auto" }}>
+                                <Button variant="outline-success" size="sm" onClick={handleSave}>เพิ่มสินค้า</Button>
+                            </Col>
+                        </Row>
+                    </Container>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
+                    <Button variant="secondary" size="sm" onClick={handleClose}> ปิด </Button>
                 </Modal.Footer>
             </Modal>
 
