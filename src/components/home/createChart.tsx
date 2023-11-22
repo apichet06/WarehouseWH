3bbecd4023759bd42ed03994f6d4b8115cc86378
apichet06 <apichet06@gmail.com>
2023-11-22 15:18:17 +0700

    10rd commit

--- src/components/home/createChart.tsx
+++ src/components/home/createChart.tsx
@@ -1,32 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { defaults } from "chart.js/auto";
 import { Card, Col, Container, Row } from "react-bootstrap";
-import { Bar, Doughnut } from "react-chartjs-2";
-import { Chart as ChartJS } from "chart.js/auto";
-import ChartDataLabels from 'chartjs-plugin-datalabels';
-import { useEffect } from "react";
+import { Doughnut } from "react-chartjs-2";
+
 interface ChartProductProps {
     api: string;
 }
 
+defaults.plugins.title.display = true;
+defaults.plugins.title.align = "start";
+defaults.plugins.title.color = "black";
 
 export default function ChartProduct(props: ChartProductProps) {
     const { api } = props;
 
-    const initialData = {
-        labels: ['Red', 'Blue', 'Yellow'],
-        datasets: [{
-            label: 'My First Dataset',
-            data: [65, 59, 80],
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgb(255, 99, 132)',
-            borderWidth: 1
-        }]
-    };
-
-    useEffect(() => {
-        ChartJS.register(ChartDataLabels);
-
-    }, []);
     return (
         <>
 
@@ -37,7 +24,6 @@ export default function ChartProduct(props: ChartProductProps) {
                             <Card.Body>
                                 {api}
                                 <h2>จำนวนสินค้าทั้งหมด</h2>
-                                <Bar data={initialData} height="70%" />
                                 <Doughnut
                                     data={{
                                         labels: ['Red', 'Blue', 'Yellow'],
