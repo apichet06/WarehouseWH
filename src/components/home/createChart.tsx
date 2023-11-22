5b0122c1816042cc5d3fbb282bbfc0a185300bd1
apichet06 <apichet06@gmail.com>
2023-11-22 16:32:32 +0700

    10rd commit

--- src/components/home/createChart.tsx
+++ src/components/home/createChart.tsx
@@ -1,43 +1,36 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Bar } from "react-chartjs-2";
 import { Card, Col, Container, Row } from "react-bootstrap";
-import { ChartData, ChartOptions } from "chart.js/auto";
-// import ChartDataLabels from 'chartjs-plugin-datalabels';
-import axios from "axios";
+import { Bar } from "react-chartjs-2";
+import { ChartData, defaults } from "chart.js/auto";
 import { useCallback, useEffect, useState } from "react";
-
-
-interface ProductData {
-    productName: string;
-    qtyInStock: number;
-    qtyMinimumStock: number;
-}
-
-interface Incoming {
-    qtyReceived: number
-    product: {
-        productName: string;
-    }
-}
-
-interface Packing {
-    qtyWithdrawn: number,
-    product: {
-        productName: string;
-    }
-
-}
+import axios from "axios";
 
 interface ChartProductProps {
     api: string;
 }
 
+defaults.plugins.title.display = true;
+defaults.plugins.title.align = "start";
+defaults.plugins.title.color = "black";
+interface ProductData {
+    productName: string;
+    qtyInStock: number;
+    qtyMinimumStock: number;
+}
 export default function ChartProduct(props: ChartProductProps) {
     const { api } = props;
+
     const [Amount, setAmount] = useState<ProductData[]>([]);
-    const [countIncom, setCountIncom] = useState<number[]>([]);
-    const [labels, setLabels] = useState<string[]>([]);
-    const [AmountPacking, setAmountPacking] = useState<Packing[]>([]);
+    // const initialData = {
+    //     labels: ['Red', 'Blue', 'Yellow'],
+    //     datasets: [{
+    //         label: 'My First Dataset',
+    //         data: [65, 59, 80],
+    //         backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    //         borderColor: 'rgb(255, 99, 132)',
+    //         borderWidth: 1
+    //     }]
+    // };
 
     const fetchData = useCallback(async () => {
         try {
@@ -50,49 +43,6 @@ export default function ChartProduct(props: ChartProductProps) {
             console.log(error.message);
         }
     }, [api]);
-
-    const fetchIncomingData = useCallback(async () => {
-        try {
-            const response = await axios.get<{ result: Incoming[] }>(`${api}/IncomingStockAPI`);
-            if (response.status === 200) {
-                const incomingData: Incoming[] = response.data.result;
-
-                const groupedData = incomingData.reduce((acc, data) => {
-                    const productName = data.product.productName;
-                    const qtyReceived = data.qtyReceived;
-
-                    if (!acc[productName]) {
-                        acc[productName] = 0;
-                    }
-
-                    acc[productName] += qtyReceived;
-
-                    return acc;
-                }, {} as Record<string, number>);
-
-                const labels = Object.keys(groupedData);
-                const dataValues = Object.values(groupedData);
-
-                setLabels(labels);
-                setCountIncom(dataValues);
-            }
-        } catch (error) {
-            console.log((error as Error).message);
-        }
-    }, [api]);
-
-    const fetchPickings = useCallback(async () => {
-        try {
-            const response = await axios.get(`${api}/Picking_goodsDetailAPI/Chart`);
-            if (response.status === 200) {
-                const approvedItems = response.data.result.filter((d: any) => d.isApproved === 'Y');
-                setAmountPacking(approvedItems);
-            }
-        } catch (error: any) {
-            console.log(error.message);
-        }
-    }, [api]);
-
 
     const data: ChartData<'bar'> = {
         labels: Amount.map((data) => data.productName),
@@ -117,81 +67,24 @@ export default function ChartProduct(props: ChartProductProps) {
         ],
     };
 
-    const dataIncom: ChartData<'bar'> = {
-        labels: labels,
-        datasets: [
-            {
-                label: "จำนวนสินค้านำเข้า",
-                data: countIncom,
-                borderColor: 'rgb(75, 192, 192)',
-                backgroundColor: 'rgba(75, 192, 192, 0.9)',
-                borderWidth: 2,
-                order: 1
-            },
-
-        ],
-    };
-    const datapackin: ChartData<'bar'> = {
-        labels: AmountPacking.map((data) => data.product.productName),
-        datasets: [
-            {
-                label: 'จำนวนสินค้านำเข้า',
-                data: AmountPacking.map((data) => data.qtyWithdrawn),
-                borderColor: 'rgb(255, 70, 122)',
-                backgroundColor: 'rgba(255, 70, 122, 0.8)',
-                borderWidth: 1,
-                order: 1,
-            },
-        ],
-    };
-
-    const options: ChartOptions<"bar"> = {
-        scales: {
-            y: {
-                beginAtZero: true,
-            },
-        },
-        plugins: {
-            legend: {
-                display: true,
-            },
-        },
-    };
-
     // กำหนด options และใช้ ChartJS
     useEffect(() => {
         // ChartJS.register(ChartDataLabels);
         fetchData();
-        fetchIncomingData();
-        fetchPickings();
-    }, [fetchData, fetchIncomingData, fetchPickings]);
+
+    }, [fetchData,]);
+
 
     return (
         <>
-            {api}
+
             <Container>
                 <Row className="justify-content-center">
                     <Col md={12} className="mb-2">
                         <Card className="shadow">
                             <Card.Body>
                                 <h2>จำนวนสินค้าทั้งหมด</h2>
-                                <Bar data={data} options={options} height="70%" />
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md={6} className="mb-2">
-                        <Card className="shadow">
-                            <Card.Body>
-                                <h2>จำนวนสินค้านำเข้า</h2>
-                                <Bar data={dataIncom} options={options} />
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md={6} className="mb-2">
-                        <Card className="shadow">
-                            <Card.Body>
-                                <h2>จำนวนสินค้าถูกเบิก</h2>
-                                <Bar data={datapackin} options={options} />
+                                <Bar data={data} height="70%" />
                             </Card.Body>
                         </Card>
                     </Col>
