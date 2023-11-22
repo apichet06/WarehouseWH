d16894d9006921163aba92b63c1fe0fdd628ff62
apichet06 <apichet06@gmail.com>
2023-11-22 10:37:08 +0700

    10rd commit

--- src/components/home/createChart.tsx
+++ src/components/home/createChart.tsx
@@ -3,7 +3,7 @@ import { Card, Col, Container, Row } from "react-bootstrap";
 import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS, ChartOptions } from "chart.js/auto";
 import ChartDataLabels from 'chartjs-plugin-datalabels';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 interface ChartProductProps {
     api: string;
 }
@@ -12,7 +12,7 @@ interface ChartProductProps {
 export default function ChartProduct(props: ChartProductProps) {
     const { api } = props;
 
-    const data = {
+    const initialData = {
         labels: ['Red', 'Blue', 'Yellow'],
         datasets: [{
             label: 'My First Dataset',
@@ -30,7 +30,8 @@ export default function ChartProduct(props: ChartProductProps) {
             borderWidth: 1
         }]
     };
-
+    // State to hold the chart data
+    const [data, setData] = useState(initialData);
     const options: ChartOptions<"bar"> = {
         scales: {
             x: {
@@ -45,7 +46,39 @@ export default function ChartProduct(props: ChartProductProps) {
 
     useEffect(() => {
         ChartJS.register(ChartDataLabels);
+        // Update the chart data when the API prop changes
+        // For demonstration purposes, a dummy API call is simulated
+        const fetchData = async () => {
+            try {
+                // Simulate fetching data from the API
+                const newData = {
+                    labels: ['Red', 'Blue', 'Yellow'],
+                    datasets: [{
+                        label: 'My First Dataset',
+                        data: [40, 30, 60], // Update with your fetched data
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(255, 205, 86, 0.2)',
+                        ],
+                        borderColor: [
+                            'rgb(255, 99, 132)',
+                            'rgb(255, 159, 64)',
+                            'rgb(255, 205, 86)',
+                        ],
+                        borderWidth: 1
+                    }]
+                };
 
+                // Update the chart data
+                setData(newData);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        // Call the fetchData function
+        fetchData();
     }, []);
     return (
         <>
