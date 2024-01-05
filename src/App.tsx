b45ad43ee007eb41e1920323d1ffe77ea46ed99f
apichet06 <apichet06@gmail.com>
2024-01-05 08:19:03 +0700

    update Url

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ import Approved from "./components/approved/approved";
 
 function App() {
   // const api = "http://localhost:8081/WHAPI/api";
-  const api = "https://warehouseapi20231118.azurewebsites.net/api";
+  const api = "https://warehouseapi20240105074854.azurewebsites.net/api";
 
   return (
     <Router>
